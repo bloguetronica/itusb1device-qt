7fceead46c3602c2130ef0f08c27cb80437972c8
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2021-12-01 12:37:02 +0000

    Add files via upload

--- itusb1device.cpp
+++ itusb1device.cpp
@@ -1,4 +1,4 @@
-/* ITUSB1 device class for Qt - Version 3.0.1
+/* ITUSB1 device class for Qt - Version 3.1.0
    Requires CP2130 class for Qt version 2.0.0 or later
    Copyright (c) 2020-2021 Samuel Lourenço
 
@@ -85,6 +85,12 @@ void ITUSB1Device::detach(int &errcnt, QString &errstr)
     }
 }
 
+// Returns the silicon version of the CP2130 bridge
+CP2130::SiliconVersion ITUSB1Device::getCP2130SiliconVersion(int &errcnt, QString &errstr)
+{
+    return cp2130_.getSiliconVersion(errcnt, errstr);
+}
+
 // Gets the VBUS current
 // Important: SPI mode should be configured for channel 0, before using this function!
 float ITUSB1Device::getCurrent(int &errcnt, QString &errstr)
@@ -98,6 +104,12 @@ float ITUSB1Device::getCurrent(int &errcnt, QString &errstr)
     QThread::usleep(100);  // Wait 100us, in order to prevent possible errors while disabling the chip select (workaround)
     cp2130_.disableCS(0, errcnt, errstr);  // Disable the previously enabled chip select
     return currentCodeSum / (4.0 * N_SAMPLES);  // Return the average current out of "N_SAMPLES" [5] for each measurement (currentCode / 4.0 for a single reading)
+}
+
+// Returns the hardware revision of the device
+QString ITUSB1Device::getHardwareRevision(int &errcnt, QString &errstr)
+{
+    return hardwareRevision(getUSBConfig(errcnt, errstr));
 }
 
 // Gets the manufacturer descriptor from the device
@@ -186,6 +198,19 @@ void ITUSB1Device::switchUSBData(bool value, int &errcnt, QString &errstr)
 void ITUSB1Device::switchUSBPower(bool value, int &errcnt, QString &errstr)
 {
     cp2130_.setGPIO1(!value, errcnt, errstr);  // GPIO.1 corresponds to the !UPEN signal
+}
+
+// Helper function that returns the hardware revision from a given USB configuration
+QString ITUSB1Device::hardwareRevision(const CP2130::USBConfig &config)
+{
+    QString revision;
+    if (config.majrel > 1 && config.majrel <= 27) {
+        revision += QChar(config.majrel + 'A' - 2);  // Append major revision letter (a major release number value of 2 corresponds to the letter "A" and so on)
+    }
+    if (config.majrel == 1 || config.minrel != 0) {
+        revision += QString::number(config.minrel);  // Append minor revision number
+    }
+    return revision;
 }
 
 // Helper function to list devices
